bd70bfa5ce39bbffef9bd71ab05714dfb58bfa62
Jason Davis <jasondaledavis@gmail.com>
2016-09-19 11:07:43 -0400

    updated css and social icons

--- partials/social-share.php
+++ partials/social-share.php
@@ -9,13 +9,13 @@
 ?>
 <div class="social-share">
 
-<h4>Spread the <i class="fa fa-heart-o"></i> with a click below:</h4>
+<h4>Spread the <span class="genericon genericon-heart"></span> with a click below:</h4>
 
 <ul>
-    <li><a href="https://facebook.com/sharer/sharer.php?t=<?php the_title(); ?>&amp;u=<?php the_permalink(); ?>" onclick="window.open(this.href,this.target,'width=600,height=400,resizeable,scrollbars');return" target="_blank" ><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
-    <li><a href="https://twitter.com/share?text=<?php the_title(); ?> via @weareelement502&amp;url=<?php echo the_permalink(); ?>" onclick="window.open(this.href,this.target,'width=600,height=400,resizeable,scrollbars');return" target="_blank" ><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
-    <li><a href="https://plus.google.com/share?url=<?php echo get_permalink(); ?>" id="google-plus" onclick="window.open(this.href,this.target,'width=500,height=400,resizeable,scrollbars');return" target="_blank" ><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
-    <li><a href="https://www.linkedin.com/shareArticle?url=<?php echo get_permalink(); ?>" id="linkedin" onclick="window.open(this.href,this.target,'width=500,height=400,resizeable,scrollbars');return" target="_blank" ><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+    <li><a href="https://facebook.com/sharer/sharer.php?t=<?php the_title(); ?>&amp;u=<?php the_permalink(); ?>" onclick="window.open(this.href,this.target,'width=600,height=400,resizeable,scrollbars');return" target="_blank" ><span class="genericon genericon-facebook"></span></a></li>
+    <li><a href="https://twitter.com/share?text=<?php the_title(); ?> via @weareelement502&amp;url=<?php echo the_permalink(); ?>" onclick="window.open(this.href,this.target,'width=600,height=400,resizeable,scrollbars');return" target="_blank" ><span class="genericon genericon-twitter"></span></a></li>
+    <li><a href="https://plus.google.com/share?url=<?php echo get_permalink(); ?>" id="google-plus" onclick="window.open(this.href,this.target,'width=500,height=400,resizeable,scrollbars');return" target="_blank" ><span class="genericon genericon-googleplus"></span></a></li>
+    <li><a href="https://www.linkedin.com/shareArticle?url=<?php echo get_permalink(); ?>" id="linkedin" onclick="window.open(this.href,this.target,'width=500,height=400,resizeable,scrollbars');return" target="_blank" ><span class="genericon genericon-linkedin-alt"></span></a></li>
 </ul>
 
 </div>
