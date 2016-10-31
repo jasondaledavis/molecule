5f9d40badc3677c7facbb82656f369068b1430bb
Jason Davis <jasondaledavis@gmail.com>
2016-10-31 18:09:21 -0400

    updated read-more class

--- inc/tgm-plugin-activation/init.php
+++ inc/tgm-plugin-activation/init.php
@@ -5,6 +5,11 @@ function molecule_register_required_plugins() {
     $plugins = array(
 
         array(
+            'name'      => 'Content Aware Sidebars',
+            'slug'      => 'content-aware-sidebars',
+        ),
+
+        array(
             'name'      => 'WP-PageNavi',
             'slug'      => 'wp-pagenavi',
         ),
@@ -12,6 +17,31 @@ function molecule_register_required_plugins() {
         array(
             'name'      => 'SVG Support',
             'slug'      => 'svg-support',
+        ),
+
+        array(
+            'name'      => 'Customizer Custom CSS',
+            'slug'      => 'customizer-custom-css',
+        ),
+
+        array(
+            'name'      => 'Duplicate Post',
+            'slug'      => 'duplicate-post',
+        ),
+
+        array(
+            'name'      => 'Easy Google Fonts',
+            'slug'      => 'easy-google-fonts',
+        ),
+
+        array(
+            'name'      => 'Header and Footer Scripts',
+            'slug'      => 'easy-google-fonts',
+        ),
+
+        array(
+            'name'      => 'No Page Comment',
+            'slug'      => 'no-page-comment',
         ),
 
     );
