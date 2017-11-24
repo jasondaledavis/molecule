c7c2efb5fbc1b01acce66db3a11603b483054ebe
Jason Davis <jasondaledavis@gmail.com>
2017-11-24 12:22:37 -0500

    sidebar-footer.php revised

--- template-parts/sidebar-footer.php
+++ template-parts/sidebar-footer.php
@@ -7,9 +7,17 @@
  * @since Molecule 2.4.3
  */
 ?>
+
+<?php
+if ( ! is_active_sidebar( 'sidebar-footer-1' ) && ! is_active_sidebar( 'sidebar-footer-2' ) && ! is_active_sidebar( 'sidebar-footer-3' ) && ! is_active_sidebar( 'sidebar-footer-4' ) ) {
+    return;
+}
+// If we get this far, we have widgets. Let's do this.
+?>
+
         <div class="footer-widgets">
 
-            <div class="grid">
+            <div class="grid wfull">
 
                 <div class="row"> 
 
